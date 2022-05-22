341f74114c67e33c85db411a3771d38161be5d7e
Bharat Kumar <kumarbharat661@gmail.com>
2022-05-22 10:15:35 +0530

    added create new ballot modal on home page

--- pages/home.js
+++ pages/home.js
@@ -5,10 +5,12 @@ import NavBar from "../components/Home/NavBar.js";
 import { useWeb3 } from "../context/Web3Context.js";
 import { useRouter } from "next/router";
 import Head from "next/head";
+import Modal from "../components/Modal.js";
 
 const Home = () => {
   const { account, factory } = useWeb3();
   const [onGoingBallots, setOngoingBallots] = useState([]);
+  const [open, setOpen] = useState(false);
   const router = useRouter();
   useEffect(() => {
     if (account === "undefined" || account === null) {
@@ -27,35 +29,45 @@ const Home = () => {
   }, [factory]);
 
   return (
-    <div className="w-full flex flex-col items-center">
-     <Head>
-        <title>MYVOTE</title>
-        <meta property="og:title" content="My page title" key="title" />
-      </Head>
-      <NavBar />
-      <div className="w-11/12 shadow-2xl mb-6 rounded-b-xl">
-        <div className="px-10 pt-4 flex items-center justify-between w-full">
-          <div className="text-Poppins font-normal text-2xl">
-            All Running Ballots
+    <>
+      <div className="w-full flex flex-col items-center">
+        <Head>
+          <title>MYVOTE</title>
+          <meta property="og:title" content="My page title" key="title" />
+        </Head>
+        <NavBar />
+        <div className="w-11/12 shadow-2xl mb-6 rounded-b-xl">
+          <div className="px-10 pt-4 flex items-center justify-between w-full">
+            <div className="text-Poppins font-normal text-2xl">
+              All Running Ballots
+            </div>
+            <div>
+              <button
+                className="p-3 py-2 rounded-xl px-16 bg-primary font-bold text-lg text-white hover:shadow-lg transition ease-in-out"
+                onClick={() => {
+                  setOpen(true);
+                }}
+              >
+                Create A Ballot
+              </button>
+            </div>
           </div>
-          <div>
-            <button className="p-3 py-2 rounded-xl px-16 bg-primary font-bold text-lg text-white hover:shadow-lg transition ease-in-out">
-              Create A Ballot
-            </button>
+          <hr className="mt-6 mx-10 border-2 rounded-sm border-slate-100" />
+          <div className="px-10 grid grid-cols-1 md:grid-cols-2  justify-items-center gap-2">
+            {onGoingBallots.map((value, idx) => {
+              return <BallotCard key={idx} id={value} />;
+            })}
+            {onGoingBallots.length === 0 && (
+              <h1 className="m-3 text-lg font-medium">
+                Currently there are no ongoing ballots!
+              </h1>
+            )}
           </div>
         </div>
-        <hr className="mt-6 mx-10 border-2 rounded-sm border-slate-100" />
-        <div className="px-10 grid grid-cols-1 md:grid-cols-2  justify-items-center gap-2">
-          {onGoingBallots.map((value, idx) => {
-            return <BallotCard key={idx} id={value} />;
-          })}
-          {onGoingBallots.length === 0 && (
-            <h1 className="m-3 text-lg font-medium">Currently there are no ongoing ballots!</h1>
-          )}
-        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+      <Modal open={open} setOpen={setOpen} />
+    </>
   );
 };
 
